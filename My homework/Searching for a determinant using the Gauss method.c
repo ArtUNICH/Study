c4f16e5c2096ea71e3e4a2146713c81766024a8b
Artunich <artunich.jet@gmail.com>
2026-03-09 09:06:06 -0400

    Saved the unfinished code ver5

--- My homework/Searching for a determinant using the Gauss method.c
+++ My homework/Searching for a determinant using the Gauss method.c
@@ -31,25 +31,25 @@ double multiply_n_string_of_the_matrix_by_a_number_X(double** matrix, int size_o
 double adding_n_string_to_m_string_of_the_matrix(double** matrix, int size_of_matrix, int n, int m) {
     if (n < 1 || m < 1 || n > size_of_matrix || m > size_of_matrix) {
         printf("Invalid input values!\n");
-        return;
+        return 1.0;
     }
     for (int i = 0; i < size_of_matrix; i++) {
         matrix[m - 1][i] = matrix[m - 1][i] + matrix[n - 1][i];
     }
-    return 1; //matrix multiplier for the determinant
+    return 1.0; //matrix multiplier for the determinant
 }
 
 double rearrange_lines_n_and_m_in_the_matrix(double** matrix, int size_of_matrix, int n, int m) {
     if (n < 1 || m < 1 || n > size_of_matrix || m > size_of_matrix) {
         printf("Invalid input values!\n");
-        return;
+        return 1.0;
     }
     for (int i = 0; i < size_of_matrix; i++) {
         double tmp_value = matrix[n - 1][i];
         matrix [n - 1][i] = matrix[m - 1][i];
         matrix[m - 1][i] = tmp_value;
     }
-    return -1; //matrix multiplier for the determinant
+    return -1.0; //matrix multiplier for the determinant
 }
 
 double making_the_matrix_triangular(double** matrix, int size_of_matrix) {
@@ -67,12 +67,8 @@ double making_the_matrix_triangular(double** matrix, int size_of_matrix) {
         if (matrix[i][j] == 0) {
             for (int next = i + 1; next < size_of_matrix; next++) {
                 if (matrix[next][j] != 0) {
-                    double tmp_value = matrix[i][j];
-                    matrix_multiplier_for_the_determinant *= multiply_n_string_of_the_matrix_by_a_number_X(matrix, size_of_matrix, i + 1, matrix[next][j]);
-                    matrix_multiplier_for_the_determinant *= multiply_n_string_of_the_matrix_by_a_number_X(matrix, size_of_matrix, next + 1, -tmp_value);
+                    matrix_multiplier_for_the_determinant *= multiply_n_string_of_the_matrix_by_a_number_X(matrix, size_of_matrix, next + 1, -matrix[i][j]/matrix[next][j]);
                     matrix_multiplier_for_the_determinant *= adding_n_string_to_m_string_of_the_matrix(matrix, size_of_matrix, i + 1, next + 1);
-                    matrix[i][j] = tmp_value;
-                    
                 }
             }
         }
@@ -81,7 +77,7 @@ double making_the_matrix_triangular(double** matrix, int size_of_matrix) {
 }
 
 double calculate_the_determinant(double** matrix, int size_of_matrix) {
-    doubel matrix_multiplier_for_the_determinant = making_the_matrix_triangular(matrix, size_of_matrix);
+    double matrix_multiplier_for_the_determinant = making_the_matrix_triangular(matrix, size_of_matrix);
     double result = 1;
     for (int i = 0; i < size_of_matrix; i++) {
         result *= matrix[i][i];
@@ -96,3 +92,4 @@ int main() {
     double determinant = calculate_the_determinant(matrix, size_of_matrix);
     printf("%lf\n", determinant);
     return 0;
+}
